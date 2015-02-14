0150ce31b8e3d15cb56ccdd4feead5b0ee3773b7
hcs-svn <hcs-svn@users.noreply.github.com>
2015-02-14 17:18:35 +0100

    Driver V1.02: non blocking GAD refresh

--- driver/io_fhem.js
+++ driver/io_fhem.js
@@ -9,20 +9,21 @@
  *
  * This driver has enhancements for using smartVISU with FHEM
  */
- 
+
 var io = {
   // --- Driver configuration ----------------------------------------------------
   logLevel:        1,
-  gadFilter:       "",  // regex, to hide these GADs to FHEM
-  addonDriverFile: "",   // filename of an optional addon driver
+  gadFilter:       "", // regex, to hide these GADs to FHEM
+  addonDriverFile: "",  // filename of an optional addon driver
   // -----------------------------------------------------------------------------
 
   // -----------------------------------------------------------------------------
   // P U B L I C   F U N C T I O N S
   // -----------------------------------------------------------------------------
+  driverVersion: "1.02",
   address: '',
   port: '',
-  
+
   /**
    * Does a read-request and adds the result to the buffer
    *
@@ -38,10 +39,24 @@ var io = {
    * @param      the item
    * @param      the value
    */
-  write: function (item, val) {
-    io.log(1, "write (item=" + item + " val=" + val + ")");
-    io.send({'cmd': 'item', 'id': item, 'val': val});
-    io.updateWidget(item, val);
+  write: function (gad, val) {
+    var isOwn = false;
+    if(io.gadFilter) {
+      var re = new RegExp(io.gadFilter);
+      isOwn = re.test(gad);
+    }
+
+    if(isOwn) {
+      if(io.addon) {
+        io.addon.write(gad, val);
+      }
+    }
+    else {
+      io.log(2, "write (gad=" + gad + " val=" + val + ")");
+      io.send({'cmd': 'item', 'id': gad, 'val': val});
+    }
+    io.updateWidget(gad, val);
+
   },
 
   /**
@@ -51,21 +66,34 @@ var io = {
    * @param      the value
    */
   trigger: function (name, val) {
-    // not supported
-    io.log(1, "trigger");
+    // fronthem does not want to get trigger, so simple send it the addon driver
+    if(io.addon) {
+      io.addon.trigger(name, val);
+    }
   },
 
   /**
-   * Initializion of the driver
+   * Initialization of the driver
    *
    * @param      the ip or url to the system (optional)
    * @param      the port on which the connection should be made (optional)
    */
   init: function (address, port) {
-    io.log(1, "init (address=" + address + " port=" + port + ")");
+    io.log(1, "init [V" + io.driverVersion + "] (address=" + address + " port=" + port + ")");
     io.address = address;
     io.port = port;
     io.open();
+
+    if (io.addonDriverFile) {
+      $.getScript("driver/" + io.addonDriverFile)
+        .done(function (script, status) {
+          io.addon = new addonDriver();
+          io.addon.init(io);io.addon.run();
+        })
+        .fail(function (hdr, settings, exception) {
+          io.addon = null;
+        });
+    }
   },
 
 
@@ -74,12 +102,17 @@ var io = {
    */
   run: function (realtime) {
     io.log(1, "run (readyState=" + io.socket.readyState + ")");
+
+    if(io.addon) {
+      io.addon.run();
+    }
+
     if (io.socket.readyState > 1) {
       io.open();
     }
     else {
       // old items
-      widget.refresh();
+      io.refreshWidgets();
 
       // new items
       io.monitor();
@@ -90,9 +123,10 @@ var io = {
   // -----------------------------------------------------------------------------
   // P R I V A T E   F U N C T I O N S
   // -----------------------------------------------------------------------------
-  version: '0.1',
+  protocolVersion: '0.1',
   socket: null,
   rcTimer: null,
+  addon: null,
 
   log: function (level, text) {
     if (io.logLevel >= level) {
@@ -100,6 +134,9 @@ var io = {
     }
   },
 
+  /**
+   * Start timer
+   */
   startReconnectTimer: function () {
     if (!io.rcTimer) {
       io.log(1, "Reconnect timer started");
@@ -114,6 +151,9 @@ var io = {
     }
   },
 
+  /**
+   * Stop timer
+   */
   stopReconnectTimer: function () {
     if (io.rcTimer) {
       clearInterval(io.rcTimer);
@@ -122,8 +162,44 @@ var io = {
     }
   },
 
+  /**
+   * Refresh the widgets
+   * The native SmartVISU method is blocking
+   * Therefore we handle this here in a non blocking way
+   */
+  refreshWidgets: function () {
+    $('[id^="' + $.mobile.activePage.attr('id') + '-"][data-item]').each(function (idx) {
+      setTimeout(function(item) {
+        var values = widget.get(widget.explode($(item).attr('data-item')));
+        if (widget.check(values)) {
+          $(item).trigger('update', [values]);
+        }
+      }, 2, this);
+    })
+  },
+
+  /**
+   * Update a widget
+   *
+   */
   updateWidget: function(item, val) {
-    widget.update(item, val);
+    setTimeout(function(i, v) {
+      widget.update(i, v);
+      io.log(2, "item updated: " + i + " val: " + v);
+    }, 1, item, val);
+ },
+
+
+  /**
+   * Update a plot
+   *
+   */
+  updatePlot: function(item, series) {
+    setTimeout(function(i, v) {
+      widget.update(i, v);
+      io.log(2, "series updated: " + i + " size: " + v.length);
+
+    }, 1, item, series);
   },
 
   /**
@@ -133,9 +209,9 @@ var io = {
     io.socket = new WebSocket('ws://' + io.address + ':' + io.port + '/');
 
     io.socket.onopen = function () {
-      io.log(1, "socket.onopen()");
+      io.log(2, "socket.onopen()");
       io.stopReconnectTimer();
-      io.send({'cmd': 'proto', 'ver': io.version});
+      io.send({'cmd': 'proto', 'ver': io.protocolVersion});
       io.monitor();
       if (notify.exists()) {
         notify.remove();
@@ -145,6 +221,7 @@ var io = {
 
     io.socket.onmessage = function (event) {
       var item, val;
+
       var data = JSON.parse(event.data);
       io.log(2, "onmessage() data= " + event.data);
       switch (data.cmd) {
@@ -152,13 +229,16 @@ var io = {
           location.reload(true);
           break;
 
-        case 'item': // item
+        case 'item':
           for (var i = 0; i < data.items.length; i = i + 2) {
             item = data.items[i];
             val = data.items[i + 1];
             io.updateWidget(item, val);
-            io.log(2, "item updated: " + item + " val: " + val);
           }
+          break;
+
+        case 'series':
+          ////io.updatePlot("plot.LivingRoom", series);
           break;
 
         case 'dialog':
@@ -167,8 +247,8 @@ var io = {
 
         case 'proto':
           var proto = data.ver;
-          if (proto != io.version) {
-            notify.info('Driver: fhem', 'Protocol mismatch<br />Driver is at version v' + io.version + '<br />fhem is at version v' + proto);
+          if (proto != io.protocolVersion) {
+            notify.info('Driver: fhem', 'Protocol mismatch<br />Driver is at version v' + io.protocolVersion + '<br />fhem is at version v' + proto);
           }
           break;
 
@@ -199,50 +279,99 @@ var io = {
   },
 
   /**
-   * Monitors the items
+   *  Slit the GADs into the FHEM-part and our own part
    */
-  monitor: function () {
-    if (io.socket.readyState == 1) {
-      var gads = widget.listeners();
-      io.log(1, "Total-GADs: " + gads.length);
-      var fhemGADs = [];
-      var ownGADs = [];
-      if(io.gadFilter) {
-        var re = new RegExp(io.gadFilter);
-        for (var i=0; i < gads.length; i++) {
-          var l = gads[i];
-          var own = re.test(l);
-          if(own){
-            ownGADs.push(l);
+  splitGADs: function() {
+    io.ownGADs = [];
+    io.fhemGADs = [];
+    var gads = widget.listeners();
+    if(io.gadFilter) {
+      var re = new RegExp(io.gadFilter);
+      for (var i=0; i < gads.length; i++) {
+        var gad = gads[i];
+        var own = re.test(gad);
+        if(own){
+          io.ownGADs.push(gad);
+        }
+        else {
+          io.fhemGADs.push(gad);
+        }
+      }
+    }
+    else {
+      io.fhemGADs = gads;
+    }
+  },
+
+  /**
+   *  Slit the plots into the FHEM-part and our own part
+   */
+  splitPlots: function() {
+    io.ownSeries = [];
+    io.fhemSeries = [];
+    var plots = widget.plot();
+
+    widget.plot().each(function (idx) {
+      var dataItem = $(this).attr('data-item');
+      var list = dataItem.split(',');
+
+      for (var i = 0; i < list.length; i++) {
+        var entryParts = list[i].split(".");
+        var gad = "";
+        for (var j = 0; j < entryParts.length - 3; j++) {
+          gad += ((gad.length == 0 ? "" : ".") + entryParts[j]).trim();
+        }
+
+        var plotInfo = {
+          'gad': gad,
+          'mode': entryParts[entryParts.length - 3],
+          'start': entryParts[entryParts.length - 2],
+          'end': entryParts[entryParts.length - 1]
+        };
+
+        if (io.gadFilter) {
+          var re = new RegExp(io.gadFilter);
+          if (re.test(plotInfo.gad)) {
+            io.ownSeries.push(plotInfo);
           }
           else {
-            fhemGADs.push(l);
+            io.fhemSeries.push(plotInfo);
           }
         }
-        io.log(1, "FHEM GADs: " + fhemGADs.length);
-        io.log(1, "Own GADs: " + ownGADs.length);
-      }
-      else {
-        fhemGADs = gads;
-      }
-
-      if (fhemGADs.length) {
-        io.send({'cmd': 'monitor', 'items': fhemGADs});
-      }
-
-      if (ownGADs.length) {
-        if (io.addonDriverFile) {
-          $.getScript("driver/" + io.addonDriverFile)
-            .done(function (script, status) {
-              io.log(1, io.addonDriverFile + " loaded");
-              io.addon = new addonDriver();
-              io.addon.run(io, ownGADs);
-            })
-            .fail(function (hdr, settings, exception) {
-              io.addon = null;
-            });
+        else {
+          io.fhemSeries.push(plotInfo);
         }
       }
+    });
+
+  },
+
+  /**
+   * Monitors the items
+   */
+  fhemGADs  : [],
+  ownGADs   : [],
+  fhemSeries : [],
+  ownSeries  : [],
+  monitor: function () {
+    if (io.socket.readyState == 1) {
+      io.splitGADs();
+      io.splitPlots();
+      io.log(1, "monitor (GADs:" + io.fhemGADs.length + ", Series:" + io.fhemSeries.length + ")");
+
+      if (io.fhemGADs.length) {
+        io.send({'cmd': 'monitor', 'items': io.fhemGADs});
+      }
+
+      if (io.fhemSeries.length) {
+        ////io.send({'cmd': 'series', 'items': io.fhemSeries});
+      }
+
+      if (io.addon) {
+        io.addon.monitor(io.ownGADs, io.ownSeries, []);
+      }
+
+
     }
   },
 
@@ -255,11 +384,9 @@ var io = {
       io.socket.close();
       io.socket = null;
       io.log(1, "socket closed");
-
-      if(io.addon) {
-        io.addon.stop();
-      }
     }
+
   }
 
 };
+
