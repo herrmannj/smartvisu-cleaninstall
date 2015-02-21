51723e8d1246d56b0d1d02627d53459a911fe855
Bernd Gewehr <bgewehr@users.noreply.github.com>
2015-02-21 21:21:38 +0100

    Update widget.js
    Corrected icon.battery for accepting min parameter.

--- widgets/widget.js
+++ widgets/widget.js
@@ -946,7 +946,7 @@ $(document).delegate('svg[data-widget="icon.battery"]', {
 	'update': function (event, response) {
 		// response is: {{ gad_value }}, {{ gad_switch }}
 
-		var val = Math.round(response[0] / $(this).attr('data-max') * 40);
+		var val = Math.round((response[0] - $(this).attr('data-min')) / ($(this).attr('data-max') - $(this).attr('data-min')) * 40);
 		fx.grid(this, val, [39, 68], [61, 28]);
 	}
 });
